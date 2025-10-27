b21bed8bd423934cdfc1199d55721c4a71124d62
gustavoluiz15 <costa.gustavo15@escola.pr.gov.br>
2025-10-27 19:45:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,44 +2,52 @@ const artistas = [
   {
     nome: "Ariana Grande",
     genero: "Pop / R&B",
-    descricao: "faixas e hits em destaque",
+    descricao: "Faixas e hits em destaque",
+    imagem: "https://i.imgur.com/xF2rJwW.jpg",
     spotify: "https://open.spotify.com/embed/artist/66CXWjxzNUsdJxJ2JdwvnR"
   },
   {
     nome: "Lana Del Rey",
     genero: "Indie / Dream pop",
-    descricao: "atmosfera cinematográfica",
+    descricao: "Atmosfera cinematográfica",
+    imagem: "https://i.imgur.com/Z2XZBhz.jpg",
     spotify: "https://open.spotify.com/embed/artist/00FQb4jTyendYWaN8pK0wa"
   },
   {
     nome: "Taylor Swift",
     genero: "Pop / Country",
-    descricao: "discografia icônica",
+    descricao: "Discografia icônica",
+    imagem: "https://i.imgur.com/ZgQW93I.jpg",
     spotify: "https://open.spotify.com/embed/artist/06HL4z0CvFAxyc27GXpf02"
   },
   {
     nome: "Billie Eilish",
     genero: "Alternative pop",
-    descricao: "som intimista",
+    descricao: "Som intimista e experimental",
+    imagem: "https://i.imgur.com/tY2Lk3E.jpg",
     spotify: "https://open.spotify.com/embed/artist/6qqNVTkY8uBg9cP3Jd7DAH"
   },
   {
     nome: "The Weeknd",
-    genero: "Pop / R&B",
-    descricao: "produção impecável e som noturno",
+    genero: "Pop / Synthwave / R&B",
+    descricao: "Produção impecável e som noturno",
+    imagem: "https://i.imgur.com/qn1sFqG.jpg",
     spotify: "https://open.spotify.com/embed/artist/1Xyo4u8uXC1ZmMpatF05PJ"
   }
 ];
 
-const container = document.getElementById("listaArtistas");
+const container = document.getElementById("artistas");
 
 artistas.forEach(artista => {
   const card = document.createElement("div");
   card.classList.add("card");
   card.innerHTML = `
-    <h2>${artista.nome}</h2>
-    <p>${artista.genero} — ${artista.descricao}</p>
-    <iframe src="${artista.spotify}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <img src="${artista.imagem}" alt="${artista.nome}">
+    <div class="card-content">
+      <h2>${artista.nome}</h2>
+      <p>${artista.genero} — ${artista.descricao}</p>
+      <iframe src="${artista.spotify}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    </div>
   `;
   container.appendChild(card);
 });
